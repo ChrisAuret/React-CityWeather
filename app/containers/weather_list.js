4279aed985023992add3bb189ff72f30b2e2f080
Chris Auret <chris.auret@yahoo.co.uk>
2016-05-02 00:34:31 +1000

    Add city names to weather_list

--- app/containers/weather_list.js
+++ app/containers/weather_list.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-class WeatherList extends Component{
+class WeatherList extends Component{    
+    renderWeather(cityData) {
+        const name = cityData.city.name;
+        return(
+            <tr key={name}>
+                <td>{name}</td>
+            </tr>
+        )
+    }
     
-    render(){        
+    render() {        
         return ( 
             <table className="table table-hover">
                 <thead>
                     <tr>
                         <th>City</th>
                         <th>Tempreture</th>
-                        <th>Pressur</th>
+                        <th>Pressure</th>
                         <th>Humidity</th>
                     </tr>
                 </thead>
                 <tbody>
+                {this.props.weather.map(this.renderWeather)}
                 </tbody>
             </table>
         );
