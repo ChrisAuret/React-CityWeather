8d5b9efb0e9e2c3f72f53643a9c470ff5dd000cb
Chris Auret <chris.auret@yahoo.co.uk>
2016-05-02 15:39:13 +1000

    Added a comment

--- app/actions/index.js
+++ app/actions/index.js
@@ -3,6 +3,7 @@ const API_KEY = "7f690c6f38e9a6c54ffe54a3c3c6c115";
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
+// this is the action-creator
 export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},us`;
     const request = axios.get(url);
