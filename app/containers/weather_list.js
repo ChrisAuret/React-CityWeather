a949b89238476ce09932181a4f35334fb66b92c8
Chris Auret <chris.auret@yahoo.co.uk>
2016-05-02 14:51:52 +1000

    Add a map for each city

--- app/containers/weather_list.js
+++ app/containers/weather_list.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 
 class WeatherList extends Component{    
     renderWeather(cityData) {
         const name = cityData.city.name;
-        const temps = cityData.list.map(weather => weather.main.temp);
+        const temps = cityData.list.map(weather => weather.main.temp - 273.15 );
         const pressures = cityData.list.map(weather => weather.main.pressure);
         const humidities = cityData.list.map(weather => weather.main.humidity);
+        //const lon = cityData.city.data.coord.lon;
+        //const lat = cityData.city.data.coord.lat;
+        const {lon, lat} = cityData.city.coord;
         
         return (
             <tr key={name}>
-                <td>{name}</td>
-                <td><Chart color={"red"} data={temps} units="K" /></td>
+                <td><GoogleMap lon={lon} lat={lat} /></td>
+                <td><Chart color={"red"} data={temps} units="°C" /></td>
                 <td><Chart color={"green"} data={pressures} units="hPa"/></td>
                 <td><Chart color={"blue"} data={humidities} units="%" /></td>
             </tr>
@@ -26,7 +30,7 @@ class WeatherList extends Component{
                 <thead className="thead-inverse">
                     <tr>
                         <th>City</th>
-                        <th>Tempreture (K)</th>
+                        <th>Tempreture (°C)</th>
                         <th>Pressure (hPa)</th>
                         <th>Humidity (%)</th>
                     </tr>
